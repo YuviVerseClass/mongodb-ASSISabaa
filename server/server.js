b5cf1ed000060ea3539b140fa66f69773b6b1aac
Sabaa Assi <sabaa.assi2911@gmail.com>
2025-07-01 16:33:17 +0300

    Add static file serving and root route to serve index.html

--- server/server.js
+++ server/server.js
@@ -7,7 +7,14 @@ const apiRoutes = require('./routes/api');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname + '/../'));
+
 app.use('/api', apiRoutes);
+
+const path = require('path');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to MongoDB"))
@@ -15,3 +22,4 @@ mongoose.connect(process.env.MONGO_URL)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on port", PORT));
+console.log("Mongo URI:", process.env.MONGO_URL);
